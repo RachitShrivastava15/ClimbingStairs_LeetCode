202dd9748d89ec9221dda232b4a9dfda402e2f6d
RachitShrivastava <rachitshrivastava15@gmail.com>
2022-10-02 12:15:49 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ClimbingStairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 2;
+            int result = climbStairs(n);
+            Console.WriteLine(result);
+        }
+
+        static int climbStairs(int n)
+        {
+            if (n < 3)
+                return n;
+
+            int n_2 = 2;
+            int n_1 = 1;
+            int res = 0;
+
+            int i = 3;
+            while(i++ <= n)
+            {
+                res = n_2 + n_1;
+                n_1 = n_2;
+                n_2 = res;
+            }
+
+            return res;
+        }
+    }
+}
